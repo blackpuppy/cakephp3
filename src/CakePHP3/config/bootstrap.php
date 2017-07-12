36f899cf36b1de3047c8433b828c89a22a3789d8
Zhu Ming <mingzhu.z+github@gmail.com>
2017-07-13 00:42:18 +0800

    create plugin Wpadmin

--- src/CakePHP3/config/bootstrap.php
+++ src/CakePHP3/config/bootstrap.php
@@ -220,3 +220,5 @@ Type::build('timestamp')
 if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
+
+Plugin::load('Wpadmin', ['bootstrap' => false, 'routes' => true]);
---
+++ src/cakephp3/plugins/Wpadmin/src/Controller/AppController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Wpadmin\Controller;
+
+use App\Controller\AppController as BaseController;
+
+class AppController extends BaseController
+{
+
+}
